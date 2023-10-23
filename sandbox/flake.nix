f801b8e3c5212c42009a62d0971bd47df7cd1380
rydnr <github@acm-sl.org>
2023-10-23 13:18:26 +0200

    New tag 0.0.81 in https://github.com/rydnr/sandbox

--- sandbox/flake.nix
+++ sandbox/flake.nix
@@ -47,7 +47,7 @@
       let
         org = "rydnr";
         repo = "sandbox";
-        version = "0.0.80";
+        version = "0.0.81";
         sha256 = "052bc0xdscxvq6qhxyrx2j9x80skg6gkd3ijjcb99a3qfbv22kc1";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
