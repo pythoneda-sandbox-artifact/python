bb80c81da0d0ead534365f28c7acba5b677d970f
UnveilingPartner <unveilingpartner@acm-sl.org>
2023-10-24 15:00:39 +0200

    New tag 0.0.134 in https://github.com/rydnr/sandbox

--- sandbox/flake.nix
+++ sandbox/flake.nix
@@ -47,7 +47,7 @@
       let
         org = "rydnr";
         repo = "sandbox";
-        version = "0.0.133";
+        version = "0.0.134";
         sha256 = "1mx3blg5p9gkdk1lnznan2y3ccahsqfspjnzxk665f0mcn2ym5ix";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
